a6804550ed33afba5a55d12207187cd43579235f
Tristan Krass <tristankrass@gmail.com>
2019-08-11 11:52:14 +0300

    s.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from '../components/seo';
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>WOOOORKINg!</h1>
+    <h1>Hey!</h1>
     <Link to="/page-2/">Blog</Link>
   </Layout>
 );
