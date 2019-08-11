2275214cc7aec856f31f9cdbe6f0378788a54a06
Tristan Krass <tristankrass@gmail.com>
2019-08-11 19:33:59 +0300

    fixed all pages

---
+++ src/pages/uses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="Blog" />
+  </Layout>
+);
+
+export default SecondPage;
